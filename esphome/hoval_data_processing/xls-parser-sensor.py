5114741f0c693b40975a64189f10c02fb79bd397
Nicolas Liaudat <nliaudat@users.noreply.github.com>
2023-06-09 12:44:44 +0200

    Update xls-parser-sensor.py

--- esphome/hoval_data_processing/xls-parser-sensor.py
+++ esphome/hoval_data_processing/xls-parser-sensor.py
@@ -1,90 +1,64 @@
-
-#### adapted from https://github.com/zittix/Hoval-GW/tte-gw-xls-parser.py
-
 import openpyxl
 import yaml
 import os
 
-wb = openpyxl.load_workbook(filename = 'TTE-GW-Modbus-datapoints.xlsx', read_only=True)
+wb = openpyxl.load_workbook(filename='TTE-GW-Modbus-datapoints.xlsx', read_only=True)
 
-    
-ret_all = {}
-ret_sensor = {}
+ret_sensor = []
 
-def parse_and_merge(ws, ret_all, ret_sensor):
+def parse_and_merge(ws, ret_sensor):
     header_found = False
-    for row in ws.rows:
+    dpid_set = set()  # Set to store unique dpids
+    for row in ws.iter_rows(values_only=True):
         if not header_found:
             header_found = True
             continue
-        i = 0
-        dpid=[]
-        descr=None
-        all = {
-        }
-        sensor = {
-        }
-        for cell in row:
-            if i >= 3 and i <=5:
-                dpid.append(str(cell.value))
+        dpid = []
+        all_data = {}  # Dictionary to store all data
+        for i, cell_value in enumerate(row):
+            if i >= 3 and i <= 5:
+                dpid.append(str(cell_value))
             elif i == 1:
-                dpid.append(cell.value)
-                all['device'] = cell.value
-            elif i == 6: #DatapointName
-                all['descr'] = cell.value
-                substitutions = cell.value
-            elif i == 8: #TypeName
-                all['type'] = cell.value
-            elif i == 9: #Decimal
-                all['accuracy_decimals'] = cell.value
-            elif i == 10: #FunctionGroup name
-                all['function_group'] = cell.value
-            elif i == 11: #Function name
-                all['function_name'] = cell.value
-            elif i == 12: #Steps
-                all['steps'] = cell.value
-            elif i == 13: #Min. value
-                all['min'] = cell.value
-            elif i == 14: #Max. value
-                all['max'] = cell.value
-            elif i == 15: #Writable
-                all['writable'] = True if cell.value and cell.value.lower() == 'Yes' else False
-            elif i == 16: #Unit
-                all['unit'] = cell.value
-            elif i == 17: #Comment
-                all['comment'] = cell.value
-            # elif i > 17 and cell.value:
-                # all['texts'][i - 18] = {lang: cell.value }
-            #print(row)
-            i+=1
-        dpid = "-".join(dpid) #identifier like "HV-50-0-37600"
-        
-        sensor['id'] = dpid
-        sensor['name'] = "${" + dpid +"}"
-        sensor['unit_of_measurement'] = all['unit']
-        sensor['accuracy_decimals'] = all['accuracy_decimals']
+                dpid.append(cell_value)
+                all_data['device'] = cell_value
+            elif i == 6:  # DatapointName
+                all_data['descr'] = cell_value
+            elif i == 8:  # TypeName
+                all_data['type'] = cell_value
+            elif i == 9:  # Decimal
+                all_data['accuracy_decimals'] = cell_value
+            elif i == 10:  # FunctionGroup name
+                all_data['function_group'] = cell_value
+            elif i == 11:  # Function name
+                all_data['function_name'] = cell_value
+            elif i == 12:  # Steps
+                all_data['steps'] = cell_value
+            elif i == 13:  # Min. value
+                all_data['min'] = cell_value
+            elif i == 14:  # Max. value
+                all_data['max'] = cell_value
+            elif i == 15:  # Writable
+                all_data['writable'] = True if cell_value and cell_value.lower() == 'yes' else False
+            elif i == 16:  # Unit
+                all_data['unit_of_measurement'] = cell_value
+            elif i == 17:  # Comment
+                all_data['comment'] = cell_value
 
-        #ret_sensor[dpid] = sensor # add all, will be curated later
-        
-        if dpid not in ret_all:  #add only unique
-            ret_all[dpid] = all
+        dpid = "-".join(dpid)  # identifier like "HV-50-0-37600"
 
+        if dpid not in dpid_set:
+            sensor = {
+                'platform': 'template',
+                'id': dpid,
+                'name': "${" + dpid + "}",
+                'unit_of_measurement': all_data['unit_of_measurement'],
+                'accuracy_decimals': all_data.get('accuracy_decimals', 0)
+            }
 
-        if dpid not in ret_sensor:  #add only unique
-            ret_sensor[dpid] = sensor
-        # else:
-            # ret[dpid]['descr'][lang] = all['descr'][lang]
-            # ret[dpid]['function_group'][lang] = all['function_group'][lang]
-            # ret[dpid]['function_name'][lang] = all['function_name'][lang]
-            # ret[dpid]['comment'][lang] = all['comment'][lang]
-            # for i in ret[dpid]['texts'].keys():
-                # if i in all['texts']:
-                    # ret[dpid]['texts'][i][lang] = all['texts'][i][lang]
+            ret_sensor.append(sensor)
+            dpid_set.add(dpid)
 
-
-
-parse_and_merge( wb.worksheets[1], ret_all, ret_sensor) # english only
-
+parse_and_merge(wb.worksheets[1], ret_sensor)  # English only
 
 with open('sensor.yaml', 'w', encoding='utf-8') as f:
-    yaml.dump(ret_sensor, f, encoding="utf-8", indent=4)
+    yaml.dump(ret_sensor, f, encoding="utf-8", sort_keys=False)
